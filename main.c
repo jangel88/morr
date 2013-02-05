addb13e93254e95904ee27fb01888955138fd697
Jordan Angel <zjba15@goldmail.etsu.edu>
2013-02-05 08:19:19 -0500

    Fixed memory leak. Process 0 writes best solution to ga.out.

--- main.c
+++ main.c
@@ -1,20 +1,4 @@
 
-/*
- * Copyright (C) 2013 Jordan Angel
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- * You should have received a copy of the GNU
- * General Public License
- * along with this program.  If not, see
- * <http://www.gnu.org/licenses/>. */
 #include<stdio.h>
 #include<stdlib.h>
 #include"utils.c"
@@ -38,15 +22,15 @@ int main(int argc, char *argv[])
   int zcoors[19200];
   int topology[6*n*m*p];
   int i,j,k;
-  int NumOfElites=100,elites[NumOfElites]; 
+  int NumOfElites=100,elites[PopSize]; 
   int node_count; 
 
   int id,np; 
 
   MPI_Comm_rank(MPI_COMM_WORLD,&id);
   MPI_Comm_size(MPI_COMM_WORLD,&np);
-//  printf("Init!\n");
-  long idum=(long)(time(NULL)*id);
+
+  long idum=(long)id;
   if (id==0){
     node_file = fopen("/home/Spring13/ORNL/data/coords.txt","r");
     if (node_file==NULL){
@@ -85,52 +69,53 @@ int main(int argc, char *argv[])
   int* mutants[PopSize-NumOfElites];  
   int* NextGen[PopSize]; 
   for(j=0;j<MaxGen;j++){
-//  for(i=0;i<PopSize;i++){
-//    NextGen[i]=malloc(node_count*sizeof(int));
-//  }
- 
     nFit=tournament(N,Pop,PopSize,xcoors,ycoors,zcoors,topology,elites,NumOfElites);
-    if (nFit<bFit){bFit=nFit;bAssign=&Pop[elites[0]][0];}
+    if (nFit<bFit){bFit=nFit;bAssign=&Pop[elites[0]][0];} //New best solution
 
-    for(i=0;i<NumOfElites;i++){
-      NextGen[i]=&Pop[elites[i]][0];
-//      for(k=0;k<node_count;k++){
-//        NextGen[i][k]=Pop[elites[i]][k];
-//      }
+    for(i=0;i<PopSize;i++){
+      if(i<NumOfElites){ NextGen[i]=&Pop[elites[i]][0];} // Elite
+      else {free(&Pop[elites[i]][0]);}                  // Unfit
     }
     for(i=NumOfElites;i<PopSize;i++){   
-      mutants[i-NumOfElites]=malloc(node_count*sizeof(int));
+      mutants[i-NumOfElites]=malloc(node_count*sizeof(int)); //Populate 
       double r1=ran3(&idum);
       double r2=ran3(&idum);
       int rn=(int)(r2*NumOfElites);
-      if (r1>.500){ 
+      if (r1>.500){ // coin flip 
         NextGen[i]=mutate_swap(NextGen[rn],node_count,mutants[(i-NumOfElites)]); 
       }else{ 
         NextGen[i]=mutate_inject(NextGen[rn],node_count,mutants[(i-NumOfElites)]); 
       }  
     } 
     for(i=0;i<PopSize;i++){
-//    for(k=0;k<node_count;k++){ 
-//      Pop[i][k]=NextGen[i][k];   
-//    }
-//      free(NextGen[i]);
-      Pop[i]=&NextGen[i][0];
+      Pop[i]=&NextGen[i][0]; 
+    }  
+  } 
+  int* all_solutions=malloc(np*sizeof(int)); 
+  double* all_fitness=malloc(np*sizeof(double)); 
+  MPI_Gather(bAssign,1,MPI_INT,all_solutions,1,MPI_INT,0,MPI_COMM_WORLD);
+  MPI_Gather(&bFit,1,MPI_DOUBLE,all_fitness,1,MPI_DOUBLE,0,MPI_COMM_WORLD);
+  if(id==0){ 
+    double global_best=INT_MAX; 
+    int best_rank;
+    for(k=0;k<np;k++){
+      if(all_fitness[k]<global_best){global_best=all_fitness[k]; best_rank=k;}
     } 
+    printf("%d found best: %f\n",best_rank,global_best); 
+    char* out="ga.out"; 
+    FILE* outfile=fopen(out,"w");
+      fprintf(outfile,"%d's bFit:%f \n",best_rank,global_best);  
+      for(i=0;i<n;i++){
+        fprintf(outfile," %d  %d  %d  %d\n",zcoors[bAssign[n*0+i]],zcoors[bAssign[n*1+i]],zcoors[bAssign[n*2+i]],zcoors[bAssign[n*3+i]]);
+      }
+      fclose(outfile); 
+ 
     
   } 
+
+    
   
-  char out[20];
-  sprintf(out,"%d.out",id);
-  FILE* outfile=fopen(out,"w");
-  fprintf(outfile,"%d's bFit:%f \n",id,bFit);
-  for(i=0;i<n;i++){
-    fprintf(outfile," %d  %d  %d  %d\n",zcoors[bAssign[n*0+i]],zcoors[bAssign[n*1+i]],zcoors[bAssign[n*2+i]],zcoors[bAssign[n*3+i]]);
-  } 
-  fclose(outfile);
-
-
-
-  MPI_Finalize();    
+ MPI_Finalize();    
   return 0; 
 }
 
