8e44bb89fd29ce13c45f088695f9508b2e23c67d
Jordan Angel <zjba15@goldmail.etsu.edu>
2013-02-11 11:26:08 -0500

    Fixed argument types

--- source/titan_node_info.c
+++ source/titan_node_info.c
@@ -1,13 +1,13 @@
-nodeid_validity valid_nodeid(int nodeid)
+nodeid_validity valid_nodeid(nodeid nid)
 {
-  if  (nodeid>-1 && nodeid<19200){
+  if  (nid>-1 && nid<19200){
     return VALID;
   } else {
     return INVALID;
   }
 }
 
-nodeid_validity valid_node_list(int N, int* node_list)
+nodeid_validity valid_node_list(int N, *nodeid node_list)
 /* Check that node_list of length N contains only 
  * valid node ids*/
 {
