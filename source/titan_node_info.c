34d3eedc5c58d2c28f006385e20c18ecc3fa25b4
Ramanan Sankaran <sankaranr@ornl.gov>
2013-02-11 11:28:55 -0500

    Added the query function and including header files.

--- source/titan_node_info.c
+++ source/titan_node_info.c
@@ -1,3 +1,13 @@
+#include <stdlib.h>
+#include <mpi.h>
+#include "titan_node_info.h" 
+
+int init_node_info(void){
+  //Nothing to do here now. Node coords are initialized in the header file. 
+  return(0); 
+}
+
+
 nodeid_validity valid_nodeid(int nodeid)
 {
   if  (nodeid>-1 && nodeid<19200){
@@ -19,3 +29,17 @@ nodeid_validity valid_node_list(int N, int* node_list)
   } 
   return VALID; 
 }
+
+nodeid query_nodeid(){
+  int flag;
+  char name[32];
+  char* name_offset;
+  int len;
+
+  flag = MPI_Get_processor_name(name, &len);
+  if(flag != MPI_SUCCESS) return(-1);
+  name_offset=&name[3];
+  printf("%s\n", name_offset);
+  return(atoi(name_offset));
+}
+
