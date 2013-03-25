0f5c7bdc1dc3edbe770a9dcb73fcda69cdd1946c
Ramanan Sankaran <sankaranr@ornl.gov>
2013-03-24 22:30:08 -0400

    Extended the test program to print out node ids and distances between random pairs.

--- source/titan_node_info_test.c
+++ source/titan_node_info_test.c
@@ -1,13 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <mpi.h>
 #include "titan_node_info.h"
 
 int main(int argc, char*argv[]) {
-nodeid myid; 
+
+int i, j, k;
+float d; 
+int rank, size; 
+nodeid myid, *allid; 
 bool validity; 
 
 MPI_Init(&argc,&argv);
+MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
+MPI_Comm_size(MPI_COMM_WORLD, &size); 
+allid = (nodeid*) malloc(sizeof(nodeid)*size);
+srand(232); 
+
 myid=query_nodeid(); 
+MPI_Gather(&myid , sizeof(nodeid), MPI_BYTE,
+           allid, sizeof(nodeid), MPI_BYTE, 0, MPI_COMM_WORLD); 
+if(rank==0) {
+  if(valid_nodeid_list(size, allid)==false) printf("Invalid nodeid found\n"); 
+  for(i=0; i<size; i++) printf("%5d ", allid[i]); 
+  printf("\n\n"); 
+  for(i=0; i<10; i++){
+    j=rand()%size; 
+    k=rand()%size; 
+    d=distance_between_nodes(allid[j], allid[k]); 
+    printf("node %d  - node %d = %.3f \n", allid[j], allid[k], d); 
+  }
+}
+
 
 MPI_Finalize();
 return 0; 
