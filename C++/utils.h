e1054cc1b3a88d320161f50e04724ddf309760ce
Jordan Angel <zjba15@goldmail.etsu.edu>
2013-07-25 14:10:23 -0400

    Cleaning up and remove tracking from files not being used

--- C++/utils.h
+++
@@ -1,6 +0,0 @@
-typedef std::pair<float,int> fit_pair;
-
-bool comparator (const fit_pair& i, const fit_pair& j);
-float norm2(float cost[], int chromo_length);
-
-void compute_cost(float cost[], std::vector<int> chromosome, std::vector<int> topology, int chromo_length);
