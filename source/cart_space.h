b1e3877a08d44905cfe1b15e0e5a0b866bf934d9
Ramanan Sankaran <sankaranr@ornl.gov>
2013-08-14 12:10:22 -0400

    moving two methods to be private instead of public

--- source/cart_space.h
+++ source/cart_space.h
@@ -17,13 +17,14 @@ class Domain {
     int period; //period in which head to tail is valid, <=size
     int max_i,max_j,max_k;
     std::vector<int> topology;
+
+    std::vector<Subdomain> find_neighbors(Subdomain element); 
+    int get_position(Subdomain element); 
   public:
     Domain(int max_i, int max_j, int max_k);
     int get_size() { return size; }
     int get_period() { return period; } 
     Subdomain get_subdomain(int position){ return Subdomain(position, max_i, max_j, max_k); }
-    std::vector<Subdomain> find_neighbors(Subdomain element); 
-    int get_position(Subdomain element); 
     float get_fitness(std::vector<nodeid> nodelist); 
     float get_fitness(std::vector<nodeid> nodelist, std::vector<int> reorder); 
 };
