a18dc4d4e734037618d076dedd5d96f273b71e91
Ramanan Sankaran <sankaranr@ornl.gov>
2013-08-14 23:33:09 -0400

    Cleaning up the individual class

--- source/individual.C
+++ source/individual.C
@@ -1,85 +1,57 @@
-#include"individual.h"
-#include<stdio.h>
-#include<math.h>
+#include "individual.h"
+#include <stdio.h>
+#include <math.h>
 #include <algorithm>    // std::random_shuffle
  
 #define MIN(a, b) (((a) < (b)) ? (a) : (b)) /* ONLY SAFE WITH NON-FUNCTION ARGUMENTS!!*/
 #define MAX(a, b) (((a) > (b)) ? (a) : (b)) /* ONLY SAFE WITH NON-FUNCTION ARGUMENTS!!*/
 
-Individual::Individual(int length, std::vector<nodeid> nodeids, bool shuffle)
-{
+extern Domain gampi_domain;
+extern std::vector<nodeid> gampi_nodelist; 
 
-  for(std::vector<nodeid>::iterator it = nodeids.begin(); it != nodeids.end(); ++it){
-    this->chromosome.push_back(*it);
-  }
+/* ---------------------------------------------------------------------- */
+Individual::Individual(int size, bool shuffle) {
+  chromosome.resize(size);
+  for(int i=0; i<size; i++) chromosome[i]=i; 
+  if(shuffle) std::random_shuffle(chromosome.begin(),chromosome.end()); 
+  fitness=gampi_domain.get_fitness(gampi_nodelist, chromosome); 
+}
 
-  if(shuffle){
+/* ---------------------------------------------------------------------- */
+Individual::Individual(const Individual& parent, bool shuffle) {
+  chromosome.resize(parent.chromosome.size()); 
+  for(int i=0; i<chromosome.size(); i++) chromosome[i]=parent.chromosome[i]; 
+  if(shuffle) {
     std::random_shuffle(chromosome.begin(),chromosome.end());
+    fitness=gampi_domain.get_fitness(gampi_nodelist, chromosome); 
+  } else {
+    fitness=parent.fitness; 
   }
 }
 
-float Individual::give_fitness()
-{
-  return this->fitness;
+/* ---------------------------------------------------------------------- */
+Individual::Individual(const std::vector<gene>& parent_chromosome){
+  chromosome.resize(parent_chromosome.size()); 
+  for(int i=0; i<chromosome.size(); i++) chromosome[i]=parent_chromosome[i]; 
+  fitness=gampi_domain.get_fitness(gampi_nodelist, chromosome); 
 }
 
-Individual::Individual(Individual* parent, bool shuffle)
-{
-  for(std::vector<nodeid>::iterator it=parent->chromosome.begin(); it!=parent->chromosome.end(); ++it){
-    this->chromosome.push_back(*it);
-  }
-  if(shuffle){
-    std::random_shuffle(chromosome.begin(),chromosome.end());
-  }
-  fitness = parent->give_fitness();
+/* ---------------------------------------------------------------------- */
+void Individual::show() {
+  printf("%x : %8.5f : ",hash(), fitness); 
+  for(int i=0; i<chromosome.size(); i++) printf("%3d ", chromosome[i]);
+  printf("\n");
 }
 
-void Individual::show_Individual()
-{
-  for(std::vector<nodeid>::iterator it=chromosome.begin();it!=chromosome.end();++it){
-   std::cout << *it << " " ;
-  }
-  std::cout << "\n";
+/* ---------------------------------------------------------------------- */
+size_t Individual::hash() {
+  size_t results = 2166136261U; 
+  for ( int i=0; i<chromosome.size(); i++) results = 127 * results + static_cast<size_t>(chromosome[i]);
+  return results; 
 }
 
-float Individual::get_fitness(std::vector<int>* topology)
-{
-  int i,j;
-  int N=chromosome.size();
-  float tmp=0.0;
-  fitness=0.0; 
-  nodeid n1,n2;
-  std::vector<float> cost(N*6);
-
-  for(j=0;j<6;j++){
-    for(i=0;i<N;i++){
-      n1=chromosome.at(topology->at(i+j*N));
-      n2=chromosome.at(i);
-      cost[i+N*j]=distance_between_nodes(n1,n2); 
-    }
-  }
- 
-  for(i=0;i<N*6;i++){ 
-    fitness+=cost[i]*cost[i];
-//  fitness+=cost[i];
-  } 
-  fitness=sqrt(fitness);
-//fitness=fitness/(N*6);
-  return fitness;
-}
-
-size_t Individual::hash()
-{
-    size_t results = 2166136261U; 
-    for ( std::vector<nodeid>::iterator current = this->chromosome.begin(); current != this->chromosome.end(); ++ current ) {
-        results = 127 * results + static_cast<size_t>( *current );
-    }
-    return results; 
-}
-
-
-void Individual::mutate(Domain* space)
-{
+/* ---------------------------------------------------------------------- */
+void Individual::mutate() {
   float roll=1;
   while((float) rand()/RAND_MAX < roll){
     int r=rand()%3;
@@ -90,15 +62,15 @@ void Individual::mutate(Domain* space)
     }else if(r==1){
       swap_segment(mirr1,mirr2);
     }else{
-      head_to_tail(mirr1,space);
+      head_to_tail(); 
     }
     roll *= (float) 4/5; 
   }
+  fitness=gampi_domain.get_fitness(gampi_nodelist, chromosome); 
 }
 
-void Individual::swap_segment(bool mirror1, bool mirror2)
-{
- 
+/* ---------------------------------------------------------------------- */
+void Individual::swap_segment(bool mirror1, bool mirror2) {
   int length_range[10]={1,2,4,8,16,32,64,128,256,512};
   int length=length_range[rand()%5];
   int x=rand()%chromosome.size();
@@ -106,38 +78,31 @@ void Individual::swap_segment(bool mirror1, bool mirror2)
   while(x == y){ //unique segments
     x=rand()%chromosome.size();
   }
-  int N=chromosome.size();
   int start2=MAX(x,y); 
   int start1=MIN(x,y); 
 
-  while((length > (chromosome.size()-start2-1)) || //make sure we arent overshooting 
-           (start1+length > start2)){   //or overlapping
+  while( (length > (chromosome.size()-start2-1)) || //make sure we arent overshooting 
+         (start1+length > start2)){   //or overlapping
     length-=1;
   }
-  std::vector<nodeid> temp(length); 
+  std::vector<gene> temp(length); 
 
   if(mirror1){
     std::reverse_copy(chromosome.begin()+start2,chromosome.begin()+start2+length,temp.begin());
   }else{
-    std::copy(chromosome.begin()+start2,chromosome.begin()+start2+length,temp.begin());
+    std::copy        (chromosome.begin()+start2,chromosome.begin()+start2+length,temp.begin());
   }
 
   if(mirror2){
     std::reverse_copy(chromosome.begin()+start1,chromosome.begin()+start1+length,chromosome.begin()+start2);
   }else{
-    std::copy(chromosome.begin()+start1,chromosome.begin()+start1+length,chromosome.begin()+start2);
+    std::copy        (chromosome.begin()+start1,chromosome.begin()+start1+length,chromosome.begin()+start2);
   } 
   std::copy(temp.begin(),temp.end(),chromosome.begin()+start1);
-
-  if (chromosome.size()!=N){ 
-    std::cout << "Error in swap! Vector changed length\n";
-    exit(1);
-  }
 }
 
-void Individual::cut_n_paste_segment(bool mirr)
-{
-  int N=chromosome.size();
+/* ---------------------------------------------------------------------- */
+void Individual::cut_n_paste_segment(bool mirr) {
   int length_range[10]={1,2,4,8,16,32,64,128,256,512};
   int length=length_range[rand()%5]; 
   int dst=rand()%chromosome.size();
@@ -148,7 +113,7 @@ void Individual::cut_n_paste_segment(bool mirr)
     length-=1;
   }
 
-  std::vector<nodeid> temp(length); 
+  std::vector<gene> temp(length); 
   if(mirr){
     std::reverse_copy(chromosome.begin() + src , chromosome.begin() + src + length,temp.begin() ); 
   }else{
@@ -156,69 +121,18 @@ void Individual::cut_n_paste_segment(bool mirr)
   } 
   chromosome.erase(chromosome.begin() + src, chromosome.begin() + src + length);
   chromosome.insert(chromosome.begin() + dst, temp.begin(), temp.end()); 
-
-  if (chromosome.size()!=N){ 
-    std::cout << "Error in cut_n_paste! Vector changed length\n";
-    exit(1);
-  }
 }
 
-void Individual::head_to_tail(bool mirr,Domain* space)
-{ 
-  int N=chromosome.size(); 
-  int i=space->get_max_i();
-  int j=space->get_max_j();
-  int k=space->get_max_k(); 
-  int subdomains=space->get_num_subdomain();
-  int depth;
-  int num_to_cut; 
-  if(k==1){
-    depth=rand()%j; 
-    num_to_cut=depth*i;
-  }else{ 
-    depth=rand()%k; 
-    num_to_cut=i*j*depth;
-  }
-  std::vector<nodeid> temp(num_to_cut); 
+/* ---------------------------------------------------------------------- */
+void Individual::head_to_tail() { 
+  int period=gampi_domain.get_period(); 
+  int num_periods=chromosome.size()/period; 
+  if(num_periods==1) return; //nothing to do
+  int num_to_cut=(rand()%num_periods)*period;
+
+  std::vector<gene> temp(num_to_cut); 
   std::copy(chromosome.begin(),chromosome.begin()+num_to_cut,temp.begin()); 
   chromosome.erase(chromosome.begin(),chromosome.begin()+num_to_cut); 
   chromosome.insert(chromosome.end(),temp.begin(),temp.end()); 
-
-  if (chromosome.size()!=N){
-    std::cout << "Error in head2tail! Vector changed length!\n";
-    exit(1);
-  } 
 }
-
-
-
-
-
-void Individual::print_chromosome(Domain* space, int format_flag)
-{
-  int i,j,k;
-
-  int max_i = space->get_max_i();
-  int max_j = space->get_max_j();
-  int max_k = space->get_max_k();
-  if(format_flag){
-    std::cout << std::endl;
-    if (max_k == 1){
-      for(i=0;i<max_i;i++){
-        for(j=0;j<=max_j;j++){
-          if(j==max_j){
-           std::cout << std::endl;
-          }else{ 
-            printf("%4d",chromosome.at(j*max_i+i));
-          }
-        }
-      }
-    }
-  }else{
-    for(int i=0;i<chromosome.size();i++){
-      std::cout << chromosome[i] << std::endl;
-    }
-  }
-}
-
 
--- source/individual.h
+++ source/individual.h
@@ -1,17 +1,17 @@
 #include <vector>       // std::vector
-//#include "node_info.h"  //nodeid
-#include "titan_node_info.h"  //nodeid
-#include "cart_space.h" //Domain
+#include "domain.h"
 #include <boost/serialization/access.hpp>
 #include <boost/serialization/vector.hpp>
 
 class Individual  {
   private: 
+    typedef int gene; 
+    std::vector<gene> chromosome;
     double fitness;
-    std::vector<nodeid> chromosome;
+
     void swap_segment(bool mirror1, bool mirror2); 
     void cut_n_paste_segment(bool mirror); 
-    void head_to_tail(bool mirror,Domain* space); 
+    void head_to_tail(); 
     friend class boost::serialization::access;
     template<class Archive>
     void serialize(Archive & ar, const unsigned int version)
@@ -19,22 +19,16 @@ class Individual  {
       ar & fitness;
       ar & chromosome;
     }
-
   protected:
   public: 
-    Individual() { }
-    Individual(Individual* parent, bool shuffle); 
-    Individual(int length, std::vector<nodeid> nodeids, bool shuffle); 
-    void mutate(Domain*); 
-    static void crossover 
-      (Individual parent1, Individual parent2, Individual* offspring1, Individual* offspring2); 
-    float get_fitness(std::vector<int>* topology); 
-    void show_Individual(); 
-    float give_fitness();
+    Individual(int size, bool shuffle=false); 
+    Individual(const Individual& parent, bool shuffle=false); 
+    Individual(const std::vector<gene>& parent_chromosome); 
+    float get_fitness(){ return fitness;}
+    int get_size() { return chromosome.size(); }
+    void show(); 
     size_t hash(); 
-    void show_zcoors(Domain *space);
-    void print_chromosome(Domain* space, int);
-    int give_size() { return chromosome.size(); }
+    void mutate();
 };
 //namespace boost{
 //  namespace serialization{ 
