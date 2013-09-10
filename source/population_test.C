b9e7c3a67354d09d0a42b52992e97b23709e74b2
Ramanan Sankaran <sankaranr@ornl.gov>
2013-09-09 22:04:28 -0400

    Updated test for the population class

--- source/population_test.C
+++ source/population_test.C
@@ -1,85 +1,70 @@
-#include"population.h"
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<cstdlib>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include<boost/mpi.hpp>
 
+#include "population.h"
 
+Domain gampi_domain(1,1,1); 
+std::vector<nodeid> gampi_nodelist;
 
-std::vector<nodeid> get_nodes(FILE *node_file, std::vector<nodeid> node_list, int *node_count, int *max_i, int *max_j, int *max_k)
-{
-// Read in number of cores, dimensions of simulations, and node IDs assuming
-// format:
-// # comments
-// num_of_cores max_i max_j max_k
-// nid0
-// nid1
-// ...
-// nidN
+using namespace std; 
+namespace mpi = boost::mpi;
 
-  int tmp;
-  char string[100];
-  int i = 0;
+int main(int argc, char **argv){
+
+mpi::environment env(argc, argv);
+mpi::communicator world;
+std::srand(135+2*world.rank()); 
+
+int nodecount, max_i, max_j, max_k;
+if(world.rank()==0) {
+  cout << "usage:  ./individual_test.exe testfilename\n";
+  FILE* nodefile = fopen(argv[1], "r"); 
+  if (nodefile==NULL) {
+    cout << "error opening file "<<argv[1] << "\n"; 
+    return 1; 
+  }
+
+  char oneline[100];
   int line_count=0;
-  while( fgets(string,100,node_file)!=NULL){
-    if(string[0]=='#' || string[0]=='\n'){
+  while( fgets(oneline,100,nodefile)!=NULL){
+    if(oneline[0]=='#' || oneline[0]=='\n'){
+      cout << oneline; 
       continue;
     }else{
       line_count+=1;
       if(line_count==1){ 
-        sscanf(string,"%d%d%d%d",node_count,max_i,max_j,max_k); 
+        sscanf(oneline,"%d%d%d%d",&nodecount,&max_i,&max_j,&max_k); 
       }else{
-        node_list.push_back((nodeid) atoi(string));
-        i++;
+        gampi_nodelist.push_back((nodeid) atoi(oneline));
       } 
     }
   } 
-
-  return node_list; 
 }
 
+broadcast(world, max_i, 0);
+broadcast(world, max_j, 0);
+broadcast(world, max_k, 0);
+broadcast(world, gampi_nodelist, 0);
 
-int main(int argc, char **argv)
-{
-  if(argc < 2){
-    printf("usage:\n");
-    printf("./population_test N\n");
-    printf("N - population size\n");
-    exit(1);
-  }
+gampi_domain=Domain(max_i, max_j, max_k); 
 
-  int max_gen=1500;
-  int pop_size=atoi(argv[1]);
-  int num_elites=(int) pop_size*0.1;
-  int node_count, max_i, max_j, max_k; 
-  std::vector<nodeid> node_list;
-  FILE *node_file = fopen("./test_suite/chester_cart_05.txt","r");
+Individual a;
+Individual b(true); 
 
+Population p(a, 10); 
+if(world.rank()==0) p.show((char*)"ordrd1"); 
+Population q(a, 10); 
+if(world.rank()==0) q.show((char*)"ordrd2"); 
+Population r(b, 10); 
+if(world.rank()==0) r.show((char*)"shffld"); 
 
-  node_list = get_nodes(node_file,node_list,&node_count,&max_i,&max_j,&max_k); 
-  Domain space(max_i,max_j,max_k); 
-  std::vector<int> topo=space.give_topo();
-  Individual temp(node_count, node_list,true);
-  temp.get_fitness(&topo); 
+q=q+p; 
+r+=q; 
+if(world.rank()==0) r.show((char*)"additn"); 
 
-  Population P(pop_size,node_count,&temp);
-  P.set_elites(num_elites);
+Population s=r.get_unique_elites(5); 
+if(world.rank()==0) s.show((char*)"elites"); 
 
-  std::vector<Individual> elites(num_elites,temp);
-
-  std::srand(1327);
-  P.tournament(&elites,&space);
-
-
-  for(int i=0;i<max_gen;i++){
-    P.tournament(&elites,&space); 
-//  printf("%d %f %f\n",i,elites[0].give_fitness(),elites[num_elites-1].give_fitness()); 
-    P.populate_next_gen(&elites,&space);
-  }
-
-  Individual best_map=P.get_best_map(&topo);
-
-  printf("%d %f\n",pop_size,P.individuals[0].give_fitness());
-  P.individuals[0].print_chromosome(&space,1);
-  return 0;
-}
+} // end main
