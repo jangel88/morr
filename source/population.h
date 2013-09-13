a8ae2e549b1d2424079bf9b6c2db053a63de92d9
Ramanan Sankaran <sankaranr@ornl.gov>
2013-09-12 23:56:15 -0400

    Adding serialization for boost MPI communication

--- source/population.h
+++ source/population.h
@@ -1,10 +1,15 @@
 #include <vector> 
 #include "individual.h"
+#include <boost/serialization/access.hpp>
 
 class Population {
   private:
     std::vector<Individual> flock; 
-
+    friend class boost::serialization::access;
+    template<class Archive>
+    void serialize(Archive & ar, const unsigned int version) { 
+      ar & flock;
+    }
   protected:
   public: 
     Population(int size); 
