ed5cce534efafcd4c8c4d92171afecb23fec7013
Ramanan Sankaran <sankaranr@ornl.gov>
2013-09-18 12:09:59 -0400

    Cleanup, but most importantly exit criterion is broadcase to all quit at same time

--- source/population_test.C
+++ source/population_test.C
@@ -20,7 +20,7 @@ std::srand(135+2*world.rank());
 
 int nodecount, max_i, max_j, max_k;
 if(world.rank()==0) {
-  cout << "usage:  ./individual_test.exe testfilename\n";
+  cout << "usage:  ./population_test.exe testfilename\n";
   FILE* nodefile = fopen(argv[1], "r"); 
   if (nodefile==NULL) {
     cout << "error opening file "<<argv[1] << "\n"; 
@@ -56,59 +56,60 @@ if(world.rank()==0){
   a.show((char*)"Ideal soln:"); 
 }
 
-Individual b(true);
-Population e(b, 1); 
+Individual v(true);
+Population a(v, 1); //ancestor population
 
 time_t start=time(NULL); 
 time_t lastprint=time(NULL);
 int elapsed=0; 
 
 do {
+  const int subset_percent=5; //percentage of population that makes the subset for next gen
+  Population p(a, 100000); //population expanded from ancestors
+
+  //Select a subset to be used for creating next generation
+  //a = p.elitist_selection(p.get_size()*1/100); 
+  a = p.rank_selection(p.get_size()*subset_percent/100); 
 
   //Add someone else's elites to mine
   if(world.size()>1){
     int comm_offset;
     if(world.rank()==0) comm_offset=1+(rand()%(world.size()-1)); // goes from 1 to N-1
     broadcast(world, comm_offset, 0); 
-    int src_rank=(world.size()+world.rank()+comm_offset)%world.size(); // I received from
+    int src_rank=(world.size()+world.rank()+comm_offset)%world.size(); // I receive from
     int dst_rank=(world.size()+world.rank()-comm_offset)%world.size(); // I send to 
 
     mpi::request reqs[2];
-    Population r(b, 1); 
-    reqs[0]=world.isend(dst_rank, 307, e); 
+    Population s=p.elitist_selection(p.get_size()*subset_percent/100/10); 
+    Population r(v, 1);
+    reqs[0]=world.isend(dst_rank, 307, s); 
     reqs[1]=world.irecv(src_rank, 307, r); 
     mpi::wait_all(reqs, reqs+2);  
-
-    e+=r; 
+    a+=r; 
   }
-
-  Population p(e, 100000); 
-  e = p.get_unique_elites(p.get_size()*2/100); 
-
-  float my_best=e.get_best_fitness(); 
-  float world_best;
-  reduce(world, my_best, world_best, mpi::minimum<float>(), 0);
 
   time_t current=time(NULL); 
   elapsed=difftime(current, start); 
-  int sinceprint=difftime(current, lastprint); 
+  broadcast(world, elapsed, 0); //So they all stop at the same iteration
 
+  float my_best=a.get_best_fitness(); 
+  float world_best;
+  reduce(world, my_best, world_best, mpi::minimum<float>(), 0);
+  int sinceprint=difftime(current, lastprint); 
   if(world.rank()==0) {
     if(sinceprint>=1){
       printf("%3d (secs): %8.4f\n", elapsed, world_best);
       lastprint=time(NULL); 
     }
   }
-} while(elapsed<120.0);
+} while(elapsed<90); 
 
-
-
-printf("RSA printing the elites\n"); 
-if(world.rank()==0) {
-  for(int i=0; i<e.get_size(); i++){
-    Individual p = e.get_individual(i); 
-    p.show(); 
-  }
-}
+//printf("RSA printing the elites\n"); 
+//if(world.rank()==0) {
+//  for(int i=0; i<e.get_size(); i++){
+//    Individual p = e.get_individual(i); 
+//    p.show(); 
+//  }
+//}
 
 } // end main
