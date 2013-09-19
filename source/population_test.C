24d5d996ca0fbc4b010d834bdb1df131e4af7ee6
Ramanan Sankaran <sankaranr@ornl.gov>
2013-09-19 12:30:45 -0400

    Switches from elitist to rank selection when fitness goes flat, also produces the final solution

--- source/population_test.C
+++ source/population_test.C
@@ -9,21 +9,19 @@
 Domain gampi_domain(1,1,1); 
 std::vector<nodeid> gampi_nodelist;
 
-using namespace std; 
 namespace mpi = boost::mpi;
 
-int main(int argc, char **argv){
-
+int main(int argc, char **argv){//MAIN MAIN MAIN
 mpi::environment env(argc, argv);
 mpi::communicator world;
 std::srand(135+2*world.rank()); 
 
 int nodecount, max_i, max_j, max_k;
 if(world.rank()==0) {
-  cout << "usage:  ./population_test.exe testfilename\n";
+  std::cout << "usage:  ./population_test.exe testfilename\n";
   FILE* nodefile = fopen(argv[1], "r"); 
   if (nodefile==NULL) {
-    cout << "error opening file "<<argv[1] << "\n"; 
+    std::cout << "error opening file "<<argv[1] << "\n"; 
     return 1; 
   }
 
@@ -31,7 +29,7 @@ if(world.rank()==0) {
   int line_count=0;
   while( fgets(oneline,100,nodefile)!=NULL){
     if(oneline[0]=='#' || oneline[0]=='\n'){
-      cout << oneline; 
+      std::cout << oneline; 
       continue;
     }else{
       line_count+=1;
@@ -53,23 +51,34 @@ gampi_domain=Domain(max_i, max_j, max_k);
 
 if(world.rank()==0){
   Individual a;
-  a.show((char*)"Ideal soln:"); 
+  a.show((char*)"Ideal soln"); 
 }
 
 Individual v(true);
 Population a(v, 1); //ancestor population
+Population e(v, 1); //store the elites
 
 time_t start=time(NULL); 
 time_t lastprint=time(NULL);
+float currbest=v.get_fitness(); 
+std::pair<float,int> fitrank, bestfitrank; 
 int elapsed=0; 
 
 do {
   const int subset_percent=5; //percentage of population that makes the subset for next gen
+  float lastbest=currbest;
+
   Population p(a, 100000); //population expanded from ancestors
 
   //Select a subset to be used for creating next generation
-  //a = p.elitist_selection(p.get_size()*1/100); 
-  a = p.rank_selection(p.get_size()*subset_percent/100); 
+  e=p.elitist_selection(std::max(1, p.get_size()*subset_percent/100/10)); 
+  currbest=e.get_best_fitness(); 
+
+  if(currbest>lastbest*0.99) {
+    a=p.rank_selection   (std::max(1, p.get_size()*subset_percent/100)); 
+  } else {
+    a=p.elitist_selection(std::max(1, p.get_size()*subset_percent/100)); 
+  }
 
   //Add someone else's elites to mine
   if(world.size()>1){
@@ -80,9 +89,8 @@ do {
     int dst_rank=(world.size()+world.rank()-comm_offset)%world.size(); // I send to 
 
     mpi::request reqs[2];
-    Population s=p.elitist_selection(p.get_size()*subset_percent/100/10); 
     Population r(v, 1);
-    reqs[0]=world.isend(dst_rank, 307, s); 
+    reqs[0]=world.isend(dst_rank, 307, e); 
     reqs[1]=world.irecv(src_rank, 307, r); 
     mpi::wait_all(reqs, reqs+2);  
     a+=r; 
@@ -92,24 +100,25 @@ do {
   elapsed=difftime(current, start); 
   broadcast(world, elapsed, 0); //So they all stop at the same iteration
 
-  float my_best=a.get_best_fitness(); 
-  float world_best;
-  reduce(world, my_best, world_best, mpi::minimum<float>(), 0);
-  int sinceprint=difftime(current, lastprint); 
-  if(world.rank()==0) {
-    if(sinceprint>=1){
-      printf("%3d (secs): %8.4f\n", elapsed, world_best);
+  fitrank=std::make_pair(currbest, world.rank()); 
+  all_reduce(world, fitrank, bestfitrank, mpi::minimum<std::pair<float,int> >()); 
+
+  if (world.rank()==0 && difftime(current, lastprint)>=1) {
+      printf("%3d (secs): %8.5f\n", elapsed, bestfitrank.first);
       lastprint=time(NULL); 
-    }
   }
-} while(elapsed<90); 
+} while(elapsed<150); 
 
-//printf("RSA printing the elites\n"); 
-//if(world.rank()==0) {
-//  for(int i=0; i<e.get_size(); i++){
-//    Individual p = e.get_individual(i); 
-//    p.show(); 
-//  }
-//}
+Individual best; 
+if(bestfitrank.second==0 && world.rank()==0) {
+  best=e.get_individual(0); 
+  best.show((char*) "Best soln"); 
+} else if(world.rank()==bestfitrank.second) {
+  best=e.get_individual(0); 
+  world.send(0, 816, best); 
+} else if (world.rank()==0) {
+  world.recv(bestfitrank.second, 816, best); 
+  best.show((char*) "Best soln"); 
+}
 
-} // end main
+} // end MAIN MAIN MAIN
