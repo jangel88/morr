8cc049a9b98005f8ddd35cf32499ccc14aa2e154
Ramanan Sankaran <sankaranr@ornl.gov>
2014-01-13 11:49:52 -0500

    This driver program will take a list of nodes, X/Y/Z sizes and then calculate a fitness

--- source/fitness_norm_test.C
+++ source/fitness_norm_test.C
@@ -1,21 +1,39 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include "cart_space.h"
 
-
 int main(int argc, char **argv){
 
-//FILE* nodefile = fopen(argv[1], "r"); 
-//if (nodefile==NULL) {
-//  cout << "error opening file "<<argv[1] << "\n"; 
-//  return 1; 
-//}
+int max_i, max_j, max_k;
+int opt;
+
+std::cout << "Usage: fitness_norm.exe  -X max_i -Y max_j -Z max_k \n"; 
+while ((opt = getopt(argc, argv, "X:Y:Z:")) != -1) {
+  switch (opt) {
+  case 'X':
+    max_i = atoi(optarg);
+    break;
+  case 'Y':
+    max_j = atoi(optarg);
+    break;
+  case 'Z':
+    max_k = atoi(optarg);
+    break;
+  default: /* '?' */
+    fprintf(stderr, "Usage: %s [-X max_i -Y max_j -Z max_k ]\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+}
+if (optind < argc) {
+  fprintf(stderr, "Expected argument after options\n"); 
+  exit(EXIT_FAILURE);
+}
+
 FILE* nodefile = stdin;
 std::vector<nodeid> nodelist;
-
-int nodecount, max_i, max_j, max_k;
 
 char string[100];
 int line_count=0;
@@ -25,24 +43,16 @@ while( fgets(string,100,nodefile)!=NULL){
     continue;
   }else{
     line_count+=1;
-    //if(line_count==1){ 
-    //  sscanf(string,"%d%d%d%d",&nodecount,&max_i,&max_j,&max_k); 
-    //}else{
       nodelist.push_back((nodeid) atoi(string));
-    //} 
   }
 } 
-//max_i=8; max_j=8; max_k=8; //for the 512-node S3D case
-max_i=8; max_j=16; max_k=16; //for the 2048-node S3D case
 if(max_i*max_j*max_k != nodelist.size()) {
   std::cout << "Error!: maxijk != nodelist size \n"; 
   return 1;
 }
 
 float s3d_fitness   =Domain(max_i, max_j, max_k).get_fitness(nodelist); 
-float lammps_fitness=Domain(max_k, max_j, max_i).get_fitness(nodelist); 
 
-//printf("%7.3f ", s3d_fitness); 
-printf("%7.3f %7.3f ", s3d_fitness, lammps_fitness); 
+printf("%7.3f \n", s3d_fitness); 
 } // end main
 
